bf165a6df2dc9dd5a5106d456ff490aeab8cafe2
ng-webdev <gel.nikos@mailo.com>
2022-08-17 14:07:34 +0200

    Updates

---
+++ precache-manifest.30f391304a5330a18a8f20734b853a4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eda18a9a1b7b2e9ca47d",
+    "url": "/si-task/static/css/main.03cc24b1.chunk.css"
+  },
+  {
+    "revision": "eda18a9a1b7b2e9ca47d",
+    "url": "/si-task/static/js/main.eda18a9a.chunk.js"
+  },
+  {
+    "revision": "d73fd34395893faa1b27",
+    "url": "/si-task/static/css/1.91a71e73.chunk.css"
+  },
+  {
+    "revision": "d73fd34395893faa1b27",
+    "url": "/si-task/static/js/1.d73fd343.chunk.js"
+  },
+  {
+    "revision": "4263a96a1b30c809e1f2",
+    "url": "/si-task/static/js/runtime~main.4263a96a.js"
+  },
+  {
+    "revision": "e4ef91ecb7dbe948a564caedc22f071f",
+    "url": "/si-task/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/si-task/precache-manifest.30f391304a5330a18a8f20734b853a4c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/si-task/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
